8a9d2109625c4933d1a5d8cc1915b75444025d3e
shifuzzz <shifanaknavas200@gmail.com>
2025-05-06 16:34:55 +0530

    next

--- alpha.py
+++ alpha.py
@@ -1,6 +1,11 @@
-# for i in range(0,5):
-#     print(" ".join([chr(65+i)]*(i+1)))
-for i in range(65,70):
-    for j in range(65,i+1):
-         print(chr(i),end=" ")
-    print()
+# # for i in range(0,5):
+# #     print(" ".join([chr(65+i)]*(i+1)))
+# for i in range(65,70):
+#      for j in range(65,i+1):
+#           print(chr(i),end=" ")
+#      print()
+limit=int(input("enter the limit:"))
+for i in range(limit):
+     for j in range(i+1):  
+          print(chr(65+j),end=" ")
+     print()
